136fe6743cbf70bdc217b55c1896cd15c9eee22d
Will Howes <williameldenhowes@gmail.com>
2019-08-27 14:42:13 +0100

    Refactored using helper methods for making a deposit and a withdrawal

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -12,26 +12,26 @@ describe BankAccount do
 
   describe '.read_transactions' do
     it 'returns details of one transaction' do
-      @account.deposit(100, '01/01/2019')
+      make_deposit
       expect(@account.read_transactions).to eq([{ 'date' => '01/01/2019',
-                                                  'amount' => 100,
-                                                  'balance' => 100 }])
+                                                  'amount' => 1000,
+                                                  'balance' => 1000 }])
     end
 
     it 'returns details of two transactions' do
-      @account.deposit(100, '25/12/2018')
-      @account.deposit(50, '26/12/2018')
-      expect(@account.read_transactions).to eq([{ 'date' => '25/12/2018',
-                                                  'amount' => 100,
-                                                  'balance' => 100 },
+      make_deposit
+      @account.deposit(500, '26/12/2018')
+      expect(@account.read_transactions).to eq([{ 'date' => '01/01/2019',
+                                                  'amount' => 1000,
+                                                  'balance' => 1000 },
                                                 { 'date' => '26/12/2018',
-                                                  'amount' => 50,
-                                                  'balance' => 150 }])
+                                                  'amount' => 500,
+                                                  'balance' => 1500 }])
     end
 
     it 'returns details of withdrawals' do
-      @account.deposit(1000, '01/01/2019')
-      @account.withdrawal(500, '02/01/2019')
+      make_deposit
+      make_withdrawal
       expect(@account.read_transactions).to eq([{ 'date' => '01/01/2019',
                                                   'amount' => 1000,
                                                   'balance' => 1000 },
@@ -43,25 +43,39 @@ describe BankAccount do
 
   describe '.deposit' do
     it 'amends the account balance by the amount deposited' do
-      expect { @account.deposit(1000, '13/01/2012') }.to change { @account.read_current_balance }.by(1000)
+      expect { make_deposit }.to change {
+        @account.read_current_balance }.by(1000)
     end
 
     it 'raises an error if not given right number of arguments' do
       expect { @account.deposit(1000) }.to raise_error(ArgumentError)
       expect { @account.deposit }.to raise_error(ArgumentError)
-      expect { @account.deposit(1000, '13/01/2012', '01/01/2018') }.to raise_error(ArgumentError)
+      expect { @account.deposit(1000,
+              '13/01/2012', '01/01/2018') }.to raise_error(ArgumentError)
     end
   end
 
   describe '.withdrawal' do
     it 'amends the reduces the account balance by the withdrawal amount' do
-      expect { @account.withdrawal(500, '01/01/2019') }.to change { @account.read_current_balance }.by(-500)
+      expect { make_withdrawal }.to change {
+        @account.read_current_balance }.by(-500)
     end
 
     it 'raises an error if not given right number of arguments' do
       expect { @account.withdrawal(1000) }.to raise_error(ArgumentError)
       expect { @account.withdrawal }.to raise_error(ArgumentError)
-      expect { @account.withdrawal(1000, '13/01/2012', '01/01/2018') }.to raise_error(ArgumentError)
+      expect { @account.withdrawal(1000, '13/01/2012',
+        '01/01/2018') }.to raise_error(ArgumentError)
     end
   end
+end
+
+private
+
+def make_deposit
+  @account.deposit(1000, '01/01/2019')
+end
+
+def make_withdrawal
+  @account.withdrawal(500, '02/01/2019')
 end
