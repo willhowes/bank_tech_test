58c87965cf34312c9d7409ec8d1a9f4aa1367560
Will Howes <williameldenhowes@gmail.com>
2019-08-28 09:38:55 +0100

    Refactored

--- spec/bank_account_history_spec.rb
+++ spec/bank_account_history_spec.rb
@@ -3,41 +3,42 @@ require 'bank_account'
 describe BankAccount do
 
   before(:each) do
-    @account_history = BankAccount.new
+    @account = BankAccount.new
   end
 
   let(:transaction) { double :transaction, amount: nil, type: nil, date: nil }
 
-  # INSERT TESTS HERE THAT CURRENT BALANCE IS CHANGED EACH TRANSACTION
   describe '#handle_transaction' do
+    
     it 'saves the history of the transaction' do
       one_deposit
-      expect(@account_history.transaction_history[0]).to eq ({ 'date' => '01/01/2019',
+      expect(@account.transaction_history[0]).to eq ({ 'date' => '01/01/2019',
                                             'amount' => 1000.0,
                                             'type' => 'credit',
                                             'balance' => 1000.0 })
     end
+    # INSERT TESTS HERE THAT CURRENT BALANCE IS CHANGED EACH TRANSACTION
+
   end
 
-  # INSERT TESTS TO SHOW THAT TRANSACTION_HISTORY CORRECTLY RECORDS A TRANSACTION
 
   describe '.print_statement' do
 
     it 'prints a blank statement if no transactions have been made' do
-      expect(@account_history.print_statement).to eq ("date || credit || debit"\
+      expect(@account.print_statement).to eq ("date || credit || debit"\
                                                       " || balance\n"\
                                                       " || || || 0.00")
     end
 
     it 'prints a statement with details of one deposit ransaction' do
       one_deposit
-      expect(@account_history.print_statement).to eq("date || credit || debit || balance\n"\
+      expect(@account.print_statement).to eq("date || credit || debit || balance\n"\
                                                       "01/01/2019 || 1000.00 || || 1000.00")
     end
 
     it 'prints a statement with details of one withdrawal transaction' do
       one_withdrawal
-      expect(@account_history.print_statement).to eq("date || credit || debit || balance\n"\
+      expect(@account.print_statement).to eq("date || credit || debit || balance\n"\
                                                       "01/01/2019 || || 1000.00 || -1000.00")
     end
 
@@ -46,7 +47,7 @@ describe BankAccount do
       one_withdrawal
       one_deposit
       one_deposit
-      expect(@account_history.print_statement).to eq("date || credit || debit || balance\n"\
+      expect(@account.print_statement).to eq("date || credit || debit || balance\n"\
                                                       "01/01/2019 || 1000.00 || || 1000.00\n"\
                                                       "01/01/2019 || || 1000.00 || 0.00\n"\
                                                       "01/01/2019 || 1000.00 || || 1000.00\n"\
@@ -62,12 +63,12 @@ def one_deposit
   allow(transaction).to receive(:amount).and_return 1000.00
   allow(transaction).to receive(:type).and_return "credit"
   allow(transaction).to receive(:date).and_return "01/01/2019"
-  @account_history.handle_transaction(transaction)
+  @account.handle_transaction(transaction)
 end
 
 def one_withdrawal
   allow(transaction).to receive(:amount).and_return 1000.00
   allow(transaction).to receive(:type).and_return "debit"
   allow(transaction).to receive(:date).and_return "01/01/2019"
-  @account_history.handle_transaction(transaction)
+  @account.handle_transaction(transaction)
 end
